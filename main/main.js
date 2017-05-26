4a58a6a9fac448d6f4bb03a6acb45d1a1db9dee0
liyuan Huang <liyuan Huang>
2017-05-26 09:59:46 +0800

    commit main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
+module.exports = function main(x) {
   // Write your cade here
+  var sum = 0;
+  var numberArray = x.split();
+  for (var i = 0; i < numberArray.length; i++) {
+  	sum += numberArray[i];
+  }
+  return sum;
 };
